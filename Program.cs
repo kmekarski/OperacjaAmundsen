c809418485ca4e48b63e368bfaab168c2aadf44a
kmekarski <72306134+kmekarski@users.noreply.github.com>
2022-12-02 01:44:59 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace MisjaNaMarsa
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            MainMenu game = new MainMenu();
+            
+
+            game.Run();
+        }
+    }
+}
